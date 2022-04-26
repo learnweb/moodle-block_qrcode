7e4e037680274150fd377619efe90acd57330274
mahoshojoHCG <yzy688@gmail.com>
2022-04-26 15:18:42 +0800

    Add Chinese Language support.

---
+++ lang/zh_cn/block_qrcode.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains language strings used in the qrcode block
+ *
+ * @package block_qrcode
+ * @copyright 2017 T Gunkel
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = '二维码';
+$string['qrcode:addinstance'] = '添加新的二维码块';
+$string['qrcode:download'] = '显示下载按钮';
+$string['filename'] = 'course';
+$string['cachedef_qrcodes'] = '二维码的缓存';
+$string['img_tag_alt'] = '二维码';
+$string['button'] = '下载';
+$string['formats'] = '格式';
+$string['sizes'] = '大小';
+$string['settings'] = '二维码设置';
+$string['use_logo'] = '显示Logo';
+$string['use_logo_help'] = '如果复选框选中，一个Logo会显示在二维码中间。默认会显示Moodle的Logo，但是你可以上传自定义的Logo。';
+$string['logofile_png'] = '选择png格式的Logo';
+$string['logofile_svg'] = '选择svg格式的Logo';
+$string['submit'] = '保存更改';
+$string['editblock'] = '编辑二维码块';
+$string['usedefault'] = '使用默认设置';
+$string['instc_uselogo'] = '显示Logo';
+$string['privacy:metadata'] = '二维码块只显示关于课程的信息，对个人信息的储存没有任何影响。';
